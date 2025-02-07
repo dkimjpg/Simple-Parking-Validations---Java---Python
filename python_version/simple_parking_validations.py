d2b34abf0ce7670ba50d3eb6f10ade5178b70ae6
David <dkk5396@psu.edu>
2025-02-07 00:25:27 -0500

    Added some more functionality to the app by creating more GUI elements for parking validation.

--- python_version/simple_parking_validations.py
+++ python_version/simple_parking_validations.py
@@ -1,25 +1,38 @@
 import tkinter as tk
+from ctypes import windll
+windll.shcore.SetProcessDpiAwareness(1)
 
 def main_page(root):
     page = tk.Frame(root)
-    page.grid(sticky="nsew")
+    #page.grid(sticky="nsew")
+    page.grid(padx=40, pady=40)
 
-    for i in range(4):  # Configure all 4 columns
-        page.grid_columnconfigure(i, weight=1)
+    #for i in range(4):  # Configure all 4 columns
+        #page.grid_columnconfigure(i, weight=1)
     #page.grid_rowconfigure(0, weight=1)  # Give weight to rows
     #page.grid_rowconfigure(1, weight=1)
 
-    tk.Label(page, text="This is the main page").grid(row=0, column=1, columnspan=2, pady=20)
-    tk.Button(page, text="Parking Validation", justify="left", padx=10, pady=5, command=change_page).grid(row=1, column=1, sticky="nsew", padx=10)
-    tk.Button(page, text="Temporary Badge", padx=10, pady=5).grid(row=1, column=2, sticky="nsew", padx=10)
+    tk.Label(page, text="Welcome!", font=("Segoe UI", 28, "bold")).grid(row=0, column=1, columnspan=2)
+    tk.Label(page, text="Please select an option below.", font=("Segoe UI", 12, "bold")).grid(row=1, column=1, columnspan=2, pady=20)
+    tk.Button(page, text="Parking Validation", justify="left", padx=10, pady=5, command=change_page).grid(row=2, column=1, sticky="nsew", padx=10)
+    tk.Button(page, text="Temporary Badge", padx=10, pady=5).grid(row=2, column=2, sticky="nsew", padx=10)
     
     
 
 def parking_validation(root):
     page = tk.Frame(root)
     page.grid(padx=40, pady=40)
-    tk.Label(page, text="Parking Validation done here").grid(row=0)
-    tk.Button(page, text="Back to main page", justify="left", padx=10, pady=5, command=change_page).grid(row=1)
+    tk.Label(page, text="Please enter your Name and ID below.").grid(row=0, column=0, columnspan=2)
+    tk.Label(page, text="Name").grid(row=1, column=0)    
+    tk.Entry(page).grid(row=1, column=1)
+    tk.Label(page, text="ID").grid(row=2, column=0)
+    tk.Entry(page).grid(row=2, column=1)
+    tk.Button(page, text="Cancel", padx=10, pady=5, command=change_page).grid(row=3, column=0)
+    tk.Button(page, text="Submit", padx=10, pady=5).grid(row=3, column=1, sticky="e")
+    tk.Button(page, text="I have a prepaid code", padx=10, pady=5).grid(row=4, column=0, columnspan=2)
+
+def prepaid_code(root):
+    page = tk.Frame(root)
 
 def change_page():
     global page_num, root
@@ -34,7 +47,7 @@ def change_page():
 
 page_num = 1
         
-def center_window(window, width=400, height=400):
+def center_window(window, width=1000, height=720):
     # Get screen dimensions
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
