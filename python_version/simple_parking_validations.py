01ac40dc8f274dd0d947df01f80f9e9ec82caa67
David <dkk5396@psu.edu>
2025-02-08 01:43:46 -0500

    Rewrote button functions that move from page to page because the previous one wouldn't work if there were multiple buttons that led to different pages on the same page. Also imported a few libraries to get ready for csv and file system manipulation.

--- python_version/simple_parking_validations.py
+++ python_version/simple_parking_validations.py
@@ -1,8 +1,10 @@
 import tkinter as tk
+import pandas as pd
+from pathlib import Path
 from ctypes import windll
-windll.shcore.SetProcessDpiAwareness(1)
+windll.shcore.SetProcessDpiAwareness(1) #disables high dpi upscaling, which caused blurry text
 
-def main_page(root):
+def main_page(root): #home page
     page = tk.Frame(root)
     #page.grid(sticky="nsew")
     page.grid(padx=40, pady=40)
@@ -14,12 +16,12 @@ def main_page(root):
 
     tk.Label(page, text="Welcome!", font=("Segoe UI", 28, "bold")).grid(row=0, column=1, columnspan=2)
     tk.Label(page, text="Please select an option below.", font=("Segoe UI", 12, "bold")).grid(row=1, column=1, columnspan=2, pady=20)
-    tk.Button(page, text="Parking Validation", justify="left", padx=10, pady=5, command=change_page).grid(row=2, column=1, sticky="nsew", padx=10)
+    tk.Button(page, text="Parking Validation", justify="left", padx=10, pady=5, command=validation_entry).grid(row=2, column=1, sticky="nsew", padx=10)
     tk.Button(page, text="Temporary Badge", padx=10, pady=5).grid(row=2, column=2, sticky="nsew", padx=10)
     
     
 
-def parking_validation(root):
+def parking_validation(root): #employee data entry
     page = tk.Frame(root)
     page.grid(padx=40, pady=40)
     tk.Label(page, text="Please enter your Name and ID below.").grid(row=0, column=0, columnspan=2)
@@ -27,22 +29,29 @@ def parking_validation(root):
     tk.Entry(page).grid(row=1, column=1)
     tk.Label(page, text="ID").grid(row=2, column=0)
     tk.Entry(page).grid(row=2, column=1)
-    tk.Button(page, text="Cancel", padx=10, pady=5, command=change_page).grid(row=3, column=0)
-    tk.Button(page, text="Submit", padx=10, pady=5).grid(row=3, column=1, sticky="e")
+    tk.Button(page, text="Cancel", padx=10, pady=5, command=home_return).grid(row=3, column=0)
+    tk.Button(page, text="Submit", padx=10, pady=5, command=parking_submit).grid(row=3, column=1, sticky="e")
     tk.Button(page, text="I have a prepaid code", padx=10, pady=5).grid(row=4, column=0, columnspan=2)
 
-def validation_code(root):
+
+
+def prepaid_code(root): #guest data entry with prepaid codes
+    page = tk.Frame(root)
+
+
+
+def validation_code(root): #provides validation code
     page = tk.Frame(root)
     page.grid(padx=40, pady=40)
     tk.Label(page, text="Your code!").grid(row=0)
     tk.Label(page, text="insert code here").grid(row=1)
-    tk.Button(page, text="Learn more about how to use a Parking Validation Code").grid(row=2)
-    tk.Button(page, text="Return to Start").grid(row=3)
-    tk.Button(page, text="Code not working? Get a new one here.").grid(row=4)
+    tk.Button(page, text="Learn more about how to use a Parking Validation Code", padx=10, pady=5).grid(row=2)
+    tk.Button(page, text="Return to Start", padx=10, pady=5, command=home_return).grid(row=3)
+    tk.Button(page, text="Code not working? Get a new one here.", padx=10, pady=5, command=validation_entry).grid(row=4)
 
-def prepaid_code(root):
-    page = tk.Frame(root)
 
+
+"""
 def change_page():
     global page_num, root
     for widget in root.winfo_children():
@@ -53,8 +62,34 @@ def change_page():
     else:
         main_page(root)
         page_num = 1
+"""
 
-page_num = 1
+def home_return():
+    for widget in root.winfo_children():
+        widget.destroy()
+    main_page(root)
+
+def validation_entry():
+    for widget in root.winfo_children():
+        widget.destroy()
+    parking_validation(root)
+
+def prepaid_entry():
+    for widget in root.winfo_children():
+        widget.destroy()
+    prepaid_code(root)
+
+def parking_submit():
+    for widget in root.winfo_children():
+        widget.destroy()
+    validation_code(root)
+
+def prepaid_submit():
+    for widget in root.winfo_children():
+        widget.destroy()
+    validation_code(root)
+
+#page_num = 1
         
 def center_window(window, width=1000, height=720):
     # Get screen dimensions
@@ -68,8 +103,8 @@ def center_window(window, width=1000, height=720):
     # Set window size and position
     window.geometry(f"{width}x{height}+{x}+{y}")
 
-def button_clicked():
-    print("button clicked!")
+#def button_clicked():
+    #print("button clicked!")
 
 root = tk.Tk()
 root.title("Parking Validations")
