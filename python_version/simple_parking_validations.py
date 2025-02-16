150ec01e31394cd20247ef7b79fb53fadf246e28
David <dkk5396@psu.edu>
2025-02-16 00:04:44 -0500

    Fixed bugs in the prepaid_code page, mainly with mismatched labels and entries.

--- python_version/simple_parking_validations.py
+++ python_version/simple_parking_validations.py
@@ -62,33 +62,34 @@ def prepaid_code(root): #guest data entry with prepaid codes
     page.grid(padx=40, pady=40)
 
     entry_label = tk.Label(page, text="Please enter your Name, Provider, and Prepaid Code below.").grid(row=0, column=0, columnspan=2)
-    name_label = tk.Label(page, text="Name").grid(row=1, column=0)    
+    guest_label = tk.Label(page, text="Name").grid(row=1, column=0)    
+    guest_entry = tk.Entry(page)
+    guest_entry.grid(row=1, column=1)
+
+    provider_label = tk.Label(page, text="Provider").grid(row=2, column=0)
     provider_entry = tk.Entry(page)
-    provider_entry.grid(row=1, column=1)
+    provider_entry.grid(row=2, column=1)
 
-    prepaid_label = tk.Label(page, text="Prepaid Code").grid(row=2, column=0)
+    prepaid_label = tk.Label(page, text="Prepaid Code").grid(row=3, column=0)
     prepaid_entry = tk.Entry(page)
-    prepaid_entry.grid(row=2, column=1)
-
-    id_name = tk.Label(page, text="ID").grid(row=3, column=0)
-    id_entry = tk.Entry(page)
-    id_entry.grid(row=3, column=1)
+    prepaid_entry.grid(row=3, column=1)
 
     return_home = tk.Button(page, text="Cancel", padx=10, pady=5, command=home_return).grid(row=4, column=0)
-    submit_data = tk.Button(page, text="Submit", padx=10, pady=5, command= lambda: prepaid_submit(provider_entry.get(), id_entry.get(), prepaid_entry.get()))
+    submit_data = tk.Button(page, text="Submit", padx=10, pady=5, command= lambda: prepaid_submit(guest_entry.get(), provider_entry.get(), prepaid_entry.get()))
     submit_data.grid(row=4, column=1, sticky="e")
     submit_data.config(state='disabled') #initially disabled so users can't press it before filling out the entry fields for name and id
     parking_val = tk.Button(page, text="I don't have a prepaid code", padx=10, pady=5).grid(row=5, column=0, columnspan=2)
 
     def check_entries(*args):
         #Enable submit button only if all fields are filled
-        if validate_entries(name_entry, prepaid_entry, id_entry):
+        if validate_entries(guest_entry, provider_entry, prepaid_entry):
             submit_data.config(state='normal')
         else:
             submit_data.config(state='disabled')
     
-    name_entry.bind('<KeyRelease>', check_entries)
-    id_entry.bind('<KeyRelease>', check_entries)
+    guest_entry.bind('<KeyRelease>', check_entries)
+    provider_entry.bind('<KeyRelease>', check_entries)
+    prepaid_entry.bind('<KeyRelease>', check_entries)
 
 
 
